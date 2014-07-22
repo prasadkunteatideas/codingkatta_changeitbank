d694b3dac7d79c27c4c4a472d61a2809f6fc4aa3
prasadkunte <prasadkunte@gmail.com>
2014-07-22 23:32:49 +0530

    commit after sync in forked project

--- src/com/ideas/codingkatta/ChangeItBank.java
+++ src/com/ideas/codingkatta/ChangeItBank.java
@@ -2,10 +2,7 @@
 package com.ideas.codingkatta;
 public class ChangeItBank {
 	public static void main(String args[]){
-<<<<<<< HEAD
-		System.out.println("hi this is my commit");
-=======
+		System.out.println("hi this is my commit in forked project");
 		System.out.println("hi there, working on forked project using account prasad.kunte@ideas.com. bouncer");
->>>>>>> upstream/master
 	}
 }
