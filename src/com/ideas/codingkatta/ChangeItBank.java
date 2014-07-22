743719a2815045d6a7c1c6b7af68fe4fe9e0fded
prasadkunte <prasadkunte@gmail.com>
2014-07-22 22:53:35 +0530

    trying to commit

--- src/com/ideas/codingkatta/ChangeItBank.java
+++ src/com/ideas/codingkatta/ChangeItBank.java
@@ -2,6 +2,6 @@
 package com.ideas.codingkatta;
 public class ChangeItBank {
 	public static void main(String args[]){
-		System.out.println("hi");
+		System.out.println("hi this is my commit");
 	}
 }
