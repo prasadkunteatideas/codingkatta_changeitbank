8be1d9ececc625e6c4ead0c3041adec04d6983eb
prasadkunte <prasadkunte@gmail.com>
2014-07-22 21:13:50 +0530

    bouncer ! not getting anything

--- src/com/ideas/codingkatta/ChangeItBank.java
+++ src/com/ideas/codingkatta/ChangeItBank.java
@@ -2,6 +2,6 @@
 package com.ideas.codingkatta;
 public class ChangeItBank {
 	public static void main(String args[]){
-		System.out.println("hi there, working on forked project using account prasad.kunte@ideas.com");
+		System.out.println("hi there, working on forked project using account prasad.kunte@ideas.com. bouncer");
 	}
 }
